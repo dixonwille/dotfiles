3bfd561bce4be676e96aaa262d211ef0f0e36c54
Will Dixon <dixonwille@hotmail.com>
2017-06-09 18:36:59 -0400

    Added some things to Arch install script

--- .setup/installArch.sh
+++ .setup/installArch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 _font="${HOME}/.fonts"
+mkdir -p "${HOME}/.local/share/fonts"
 find $_font -type f -name "*.ttf" | while read line; do
 	ln -s $line "${HOME}/.local/share/fonts/$(basename $line)"
 done
 sudo -v
 sudo pacman -Syu atom chromium git gnupg go pinentry rxvt-unicode vim zsh
 apm install file-icons go-plus language-vue
-ln -sf /usr/bin/pinentry /usr/local/bin/pinentry
+sudo -v
+sudo ln -sf /usr/bin/pinentry /usr/local/bin/pinentry
