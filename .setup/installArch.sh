ca530ff4bf971845febc46ead2738fe6a5b0aea0
Will Dixon <dixonwille@hotmail.com>
2017-06-09 16:41:54 -0400

    Adding a few more things to install on install

--- .setup/installArch.sh
+++ .setup/installArch.sh
@@ -4,5 +4,5 @@ find $_font -type f -name "*.ttf" | while read line; do
 	ln -s $line "${HOME}/.local/share/fonts/$(basename $line)"
 done
 sudo -v
-sudo pacman -Syu git go pinentry vim zsh
+sudo pacman -Syu atom chromium git gnupg go pinentry vim zsh
 
