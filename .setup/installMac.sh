9bcf0fd059397450dbd1abe337c372f9a63a635c
Will Dixon <dixonwille@hotmail.com>
2017-08-09 14:19:43 -0400

    Install slate on Mac

--- .setup/installMac.sh
+++ .setup/installMac.sh
@@ -11,4 +11,5 @@ wallpaper "${HOME}/Images/EinsteinWP.jpg"
 brew bundle --file="${HOME}/.setup/Brewfile"
 sudo -v
 sudo ln -sf /usr/local/bin/pinentry-mac /usr/local/bin/pinentry
+cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
 source all.sh
