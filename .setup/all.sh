940edb2d44542f9dd81ed1442ba56d6e6fb7ce2e
Will Dixon <dixonwille@hotmail.com>
2017-10-23 11:30:32 -0400

    Update to use diff so fancy

--- .setup/all.sh
+++ .setup/all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apm file-icons go-plus language-vue editorconfig minimap minimap-linter
+apm install file-icons go-plus language-vue editorconfig minimap minimap-linter atom-language-rust autocomplete-racer
 chsh -s /bin/zsh
